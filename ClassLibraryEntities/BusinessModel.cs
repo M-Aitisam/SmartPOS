98bdd76aa9ecd3e07213f4e87ba65aa0d518bb6a
M-Aitisam <aitisamahmed24@gmail.com>
2025-03-03 15:10:16 +0500

    Add project files.

---
+++ ClassLibraryEntities/BusinessModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryEntities
+{
+    public class BusinessModel
+    {
+        public string? BusinessID { get; set; }
+        public string? BusinessName { get; set; }
+        public string? BusinessType { get; set; }
+        public string? City { get; set; }
+        public string? Area { get; set; }
+        public string? Location { get; set; }
+        public string? CreateAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
