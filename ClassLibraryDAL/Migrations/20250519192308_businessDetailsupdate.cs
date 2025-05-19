536d89263fda4041f2d111fc9ac623f593fc4c18
M-Aitisam <aitisamahmed24@gmail.com>
2025-05-20 00:29:45 +0500

    update the Business Registration  page add the password and confirm and also  update the full  design

---
+++ ClassLibraryDAL/Migrations/20250519192308_businessDetailsupdate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassLibraryDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class businessDetailsupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmPassword",
+                table: "BusinessDetails",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "BusinessDetails",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmPassword",
+                table: "BusinessDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "BusinessDetails");
+        }
+    }
+}
--- ClassLibraryEntities/BusinessDetails.cs
+++ ClassLibraryEntities/BusinessDetails.cs
@@ -63,6 +63,17 @@ modif.ai";
         public string AddressLine1 { get; set; } = string.Empty;
         public string AddressLine2 { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
-      
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string? ConfirmPassword { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long")]
+        public string? Password { get; set; }
+
     }
 }
