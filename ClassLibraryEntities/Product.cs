4ed8216e83d3924ccb324b3dcd90873044305c29
saadurehman32 <saadwork218@gmail.com>
2025-05-28 12:23:47 -0700

    Enhanced UI and add working functionality of export, search bar etc.

--- ClassLibraryEntities/Product.cs
+++ ClassLibraryEntities/Product.cs
@@ -1,28 +1,48 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ClassLibraryEntities
 {
     public class Product
     {
-        public string? ProductUrduName { get; set; }
-        public string? ProductTitle { get; set; }
-        public string? ProductCode { get; set; }
-        public decimal ProductPrice { get; set; }
-        public bool IsActive { get; set; } = true;
-        public string? ImageUrl { get; set; }
-        public bool IsSelected { get; set; }
-        public string ActiveDays { get; set; } = "Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday";
         public int ProductID { get; set; }
+
+        
+        public string? ProductUrduName { get; set; }
+
+        [Required(ErrorMessage = "Product title is required.")]
+        [StringLength(100, ErrorMessage = "ProductTitle cannot exceed 100 characters.")]
+        public string? ProductTitle { get; set; }
+
+       
+        public string? ProductCode { get; set; }
+
+       
+        public decimal ProductPrice { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        [Url(ErrorMessage = "Invalid URL format.")]
+        public string? ImageUrl { get; set; }
+
+        public bool IsSelected { get; set; }
+
+        [Required]
+        public string ActiveDays { get; set; } = "Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday";
+
+        
         public string Name { get; set; } = string.Empty;
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
         public decimal Price { get; set; }
 
-
+        [Required(ErrorMessage = "Category is required.")]
         public int CategoryID { get; set; }
+
         public int? SubCategoryID { get; set; }
         public int? NestedSubCategoryID { get; set; }
 
         [ForeignKey("CategoryID")]
-
         public BusinessCategory? Category { get; set; }
 
         [ForeignKey("SubCategoryID")]
