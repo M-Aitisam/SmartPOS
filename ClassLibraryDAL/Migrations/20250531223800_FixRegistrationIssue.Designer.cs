feeb56b1c775e1c5bbe07b9c8cee26a46c433c8c
M-Aitisam <aitisamahmed24@gmail.com>
2025-06-01 04:13:08 +0500

    Some Styling FIx and update

--- ClassLibraryDAL/Migrations/20250531223800_FixRegistrationIssue.Designer.cs
+++ ClassLibraryDAL/Migrations/20250531223800_FixRegistrationIssue.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ClassLibraryDAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClassLibraryDAL.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250531223800_FixRegistrationIssue")]
+    partial class FixRegistrationIssue
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.3");
